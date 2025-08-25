e58e750378abc772e40277ad780ddfbcc4b08460
earotinco <earotinco24@gmail.com>
2025-08-25 17:50:07 -0500

    Proyecto en Spring Java + dependencias

---
+++ ms-tecMedical/src/main/java/com/cib/ms_tecMedical/MsTecMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.cib.ms_tecMedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTecMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTecMedicalApplication.class, args);
+	}
+
+}
---
+++ ms-tecMedical/src/test/java/com/cib/ms_tecMedical/MsTecMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cib.ms_tecMedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTecMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
