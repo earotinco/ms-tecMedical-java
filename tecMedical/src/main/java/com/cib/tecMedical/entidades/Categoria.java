6c121a3441c44e360bbc990e714e1e06b6a7d9b8
Yadi <yadi.tp01@gmail.com>
2025-08-26 18:35:39 -0500

    agregadas clases producto y categoria

---
+++ tecMedical/src/main/java/com/cib/tecMedical/entidades/Categoria.java
@@ -0,0 +1,21 @@
+package com.cib.tecMedical.entidades;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+public class Categoria {
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long idCategoria;
+	 
+	 @Column(nullable = false)
+	 private String nombre;
+	 
+	 private String descripcion;
+	 
+	 private Boolean estado = true;
+	 
+	 @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	 private List<Producto> productos;
+}
--- tecMedical/src/main/java/com/cib/tecMedical/entidades/Producto.java
+++ tecMedical/src/main/java/com/cib/tecMedical/entidades/Producto.java
@@ -1,5 +1,30 @@
 package com.cib.tecMedical.entidades;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Table(name="Producto")
+@Data
 public class Producto {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idProducto;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	private String descripcion;
+	
+	@Column(nullable = false)
+	private double precio;
+	
+	@Column(nullable = false)
+	private Integer Stock = 0;
+	
+	private Boolean estado = true;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "IdCategoria", nullable = false)
+    private Categoria categoria;
 }
