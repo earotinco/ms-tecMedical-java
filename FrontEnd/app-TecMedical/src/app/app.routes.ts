edfbe4b9ac2a461182aad45a11b081b0029e90bb
earotinco <earotinco24@gmail.com>
2025-08-29 04:56:50 -0500

    actualizacion routes

--- FrontEnd/app-TecMedical/src/app/app.routes.ts
+++ FrontEnd/app-TecMedical/src/app/app.routes.ts
@@ -1,23 +1,30 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   { path: '', redirectTo: 'ventas', pathMatch: 'full' },
 
-{
-  path: 'ventas',
-  loadComponent: () =>
-    import('./components/venta-list/venta-list.component').then(m => m.VentaListComponent)
-},
+  {
+    path: 'ventas',
+    loadComponent: () =>
+      import('./components/venta-list/venta-list.component').then(
+        (m) => m.VentaListComponent
+      ),
+  },
+
+  {
+    path: 'ventas/nuevo',
+    loadComponent: () =>
+      import('./components/venta-form/venta-form.component').then(
+        (m) => m.VentaFormComponent
+      ),
+  },
 
   //  Ventas - Detalle
   {
     path: 'ventas/:id',
     loadComponent: () =>
-      import('./components/venta-detail/venta-detail.component').then(m => m.VentaDetailComponent)
-  }
-
-
+      import('./components/venta-detail/venta-detail.component').then(
+        (m) => m.VentaDetailComponent
+      ),
+  },
 ];
-
-
