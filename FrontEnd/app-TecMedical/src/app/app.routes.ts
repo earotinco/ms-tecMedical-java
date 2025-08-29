c6c00b4a1518b9c61c012ca982fd6ac5c94cc25d
earotinco <earotinco24@gmail.com>
2025-08-28 20:58:21 -0500

    actualizacion venta-form

--- FrontEnd/app-TecMedical/src/app/app.routes.ts
+++ FrontEnd/app-TecMedical/src/app/app.routes.ts
@@ -10,12 +10,20 @@ export const routes: Routes = [
   loadComponent: () =>
     import('./components/venta-list/venta-list.component').then(m => m.VentaListComponent)
 },
+{
+
+  path: 'ventas/nueva',
+    loadComponent: () =>
+      import('./components/venta-form/venta-form.component').then(m => m.VentaFormComponent)
+  },
+
 
 {
     path: 'ventas/:id',
     loadComponent: () =>
       import('./components/venta-detail/venta-detail.component').then(m => m.VentaDetailComponent)
   }
+
 
 
 ];
